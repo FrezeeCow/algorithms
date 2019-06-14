39b1cf9831cb83f5cd07c36d6f94faf5f6882ebe
cocoman <1750112338@qq.com>
2019-06-14 15:15:40 +0800

    Update garage.py

--- array/garage.py
+++ array/garage.py
@@ -11,15 +11,21 @@ The goal is to find out the least movement needed to rearrange the parking lot f
 
 # Say the initial state is an array:
 
-# [1,2,3,0,4],
-# where 1,2,3,4 are different cars, and 0 is the empty spot.
+# input initial: [1,2,3,0,4]  # where 1,2,3,4 are different cars, and 0 is the empty spot.
+# input final: [0, 3, 2, 1, 4]
 
-# And the final state is
+# output: 4
 
-# [0,3,2,1,4].
+
 # We can swap 1 with 0 in the initial array to get [0,2,3,1,4] and so on.
 # Each step swap with 0 only.
-# Edited by cyberking-saga
+
+# 1 2 3 0 4 --> 0 3 2 1 4
+# step1: 1 2 3 0 4 --> 0 2 3 1 4 
+# step2：0 2 3 1 4 --> 3 2 0 1 4
+# step3: 3 2 0 1 4 --> 3 0 2 1 4
+# step4: 3 0 2 1 4 --> 0 3 2 1 4
+
 
 
 def garage(initial, final):
