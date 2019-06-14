35fbd32b2350b68c764d4c563e48ad125bb26424
cocoman <1750112338@qq.com>
2019-06-14 14:57:58 +0800

    Update flatten.py

--- array/flatten.py
+++ array/flatten.py
@@ -29,34 +29,3 @@ def list_flat(l, a=None):
 
 a = [2, 1, [3, [4, 5], 6], 7, [8]]
 print(list_flatten(a))
-
-# stack version
-# public static List<Integer> flatten(List<NestedList> l) {
-# List<Integer> main = new ArrayList<Integer>();
-		# Stack<List<NestedList>> stack = new Stack<List<NestedList>>();
-		# Stack<Integer> indexes = new Stack<Integer>();
-		# stack.add(l);
-		# indexes.add(0);
-		# while (true) {
-			# if (stack.isEmpty())
-				# break;
-			# int index1 = indexes.pop();
-			# l = stack.pop();
-			# for (int i = index1; i < l.size(); i++) {
-				# NestedList n = l.get(i);
-				# if (n.isInteger()) {
-					# main.add(n.value);
-				# } else {
-					# stack.add(l);
-					# indexes.add(i+1);
-					# l = n.list;
-					# stack.add(l);
-					# indexes.add(0);
-					# break;
-
-				# }
-			# }
-		# }
-
-		# return main;
-# }
