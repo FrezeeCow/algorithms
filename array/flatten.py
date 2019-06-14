bca67087ccce7977732d6b63db87132d402391f6
cocoman <1750112338@qq.com>
2019-06-14 14:57:22 +0800

    Update flatten.py

--- array/flatten.py
+++ array/flatten.py
@@ -14,11 +14,15 @@ Output: [2, 1, 3, 4, 5, 6, 7, 8]
 """
 
 # 基于递归的实现方法，isinstance 用于判断参数a是否属于(list, tuple)中的类型的一种，返回bool类型，与type的区别在于会继承判断类型。
-def list_flatten(l, a=None):
+def list_flat(l, a=None):
+    # 如果a为None,创建一个空列表；否则将列表转为List类型
     a = list(a) if isinstance(a, (list, tuple)) else []
+    # 遍历列表项中的元素
     for i in l:
+        # 遇到仍然是列表项的进行递归
         if isinstance(i, (list, tuple)):
-            a = list_flatten(i, a)
+            a = list_flat(i, a)
+        # 遇到单个元素的加入到列表a
         else:
             a.append(i)
     return a
